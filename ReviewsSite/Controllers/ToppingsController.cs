9ce55d0074d87c329d4083fe5d3af4f1ae4b914a
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-02-17 14:46:28 -0500

    was able to build out the views folder, controllers folder, creating toppings tests.

---
+++ ReviewsSite/Controllers/ToppingsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Controllers
+{
+    public class ToppingsController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
