7632e2b59066873cf69ad9f23341d36918da8e39
Joseph Erxleben <jerxleben.13@gmail.com>
2021-02-18 13:21:09 -0500

    Added Model tests and Properties.

--- ReviewsSite/Models/Toppings.cs
+++ ReviewsSite/Models/Toppings.cs
@@ -5,8 +5,29 @@ using System.Threading.Tasks;
 
 namespace ReviewsSite.Models
 {
+
+
     public class Toppings
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVegetarian { get; set; }
+        public string Review { get; set; }
+
+        public Toppings()
+        {
+
+        }
+
+        public Toppings(int id, string name, string description, bool isVegetarian, string review)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            IsVegetarian = isVegetarian;
+            Review = review;
+        }
 
     }
 }
