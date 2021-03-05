90d61e5a71715a05e3a7bdd65a0fb212af88e461
kbh.csse@gmail.com <kbh.csse@gmail.com>
2021-03-05 11:44:00 -0500

    designed details page.

--- ReviewsSite/Models/Review.cs
+++ ReviewsSite/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace ReviewsSite.Models
     public class Review
     {
         public int ReviewId { get; set; }
+        [Display(Name = "Review" )]
         public string ReviewContent { get; set; }
+        [Display(Name = "Name")]
         public string ReviewerName { get; set; }
         public int ReviewScore { get; set; }
         public int ToppingsId { get; set; }
